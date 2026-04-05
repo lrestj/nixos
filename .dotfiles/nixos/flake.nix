0e7de5114959eaa770bd5b8d303bf8444823d48c
LrestJ <rest@seznam.cz>
2026-04-05 22:43:54 +0200

    Install script brutally edited

--- .dotfiles/nixos/flake.nix
+++ .dotfiles/nixos/flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, ... }@inputs: {
       nixosConfigurations = {
-          nixos = nixpkgs.lib.nixosSystem {
+          zbook = nixpkgs.lib.nixosSystem {
               system = "x86_64-linux";
               specialArgs = { inherit inputs; };
               modules = [
