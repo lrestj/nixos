3861ab5f7073f2c37221500f7de35d49bb059da7
LrestJ <rest@seznam.cz>
2026-04-12 10:46:23 +0200

    Fish alliases for install git added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,13 +37,17 @@ abbr hpp "NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-
 
 abbr cfg "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME"
 abbr ga "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME add"
+abbr nga "cd $HOME/.dotfiles/install/ && git add"
 abbr gc --set-cursor -- "git --git-dir $HOME/.cfg.git/ --work-tree=$HOME commit -a -m \"%\""
+abbr ngc --set-cursor -- "cd $HOME/.dotfiles/install/ && git commit -a -m \"%\""
 abbr ghp "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME push -u github main"
 abbr glp "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME push gitlab main"
 abbr ppp "git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor push -u github main && git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor push gitlab main"
+abbr npp "cd $HOME/.dotfiles/install/ && git push -u github main && git push gitlab main"
 abbr ppu "git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor pull"
 abbr gcl "git clone --bare https://codeberg.org/lrestj/nixos.git"
 abbr gs "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME status"
+abbr ngs "cd $HOME/.dotfiles/install/ && git status"
 abbr str "git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor log --pretty=format: --name-only --diff-filter=A "
 abbr untrack "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME update-index --skip-worktree"
 
