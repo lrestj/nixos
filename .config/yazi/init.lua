30cc321eab728371149258d265d41a2b1f1f391a
LrestJ <rest@seznam.cz>
2026-04-15 21:03:11 +0200

    Fix

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,6 @@
 -- ~/.config/yazi/init.lua
 require("bookmarks"):setup({
-	last_directory = { enable = true, persist = true, mode="dir" },
+	last_directory = { enable = true, persist = false, mode="dir" },
 	persist = "all",
 	desc_format = "full",
 	file_pick_mode = "hover",
