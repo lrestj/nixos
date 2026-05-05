f091a221c47917b789739123cf02ea2c32914f54
LrestJ <rest@seznam.cz>
2026-05-05 23:27:26 +0200

    Colorpicker added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+# Yazi cwd
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # Set up fzf key bindings
 fzf --fish | source
 
